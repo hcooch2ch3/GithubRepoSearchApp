fe665306afccdf469d38d214d14c74b2d04af8e0
임성민 <hcooch2ch3@gmail.com>
2022-12-12 11:50:11 +0900

    메인화면(검색화면) 구현 (#15)
    - 레포 목록 보여줄 테이블 뷰 및 테이블셀 추가
    - 검색어를 입력할 서치바 추가

---
+++ GithubRepoSearchApp/View/RepositoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryCell.swift
+//  GithubRepoSearchApp
+//
+//  Created by 임성민 on 2022/12/12.
+//
+
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- GithubRepoSearchApp/View/SearchViewController.swift
+++ GithubRepoSearchApp/View/SearchViewController.swift
@@ -11,8 +11,17 @@ class SearchViewController: UIViewController {
     
     var viewModel = SearchViewModel()
 
+    @IBOutlet weak var repositoryTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Enter Keyword"
+        self.navigationItem.searchController = searchController
+        self.navigationItem.title = "Search Github Repo"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.hidesSearchBarWhenScrolling = false
     }
 
 }
